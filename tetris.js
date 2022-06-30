1bdcd03c59134326a3830bc6c4c301cdf6dcbe78
I-the-Creator <andrey.burluckiy@gmail.com>
2022-06-30 13:36:27 +0200

    initial commit

---
+++ tetris.js
@@ -0,0 +1,33 @@
+class Tetris {
+    constructor(imageX, imageY, template) {
+        this.imageX = imageX;
+        this.imageY = imageY;
+        this.template = template;
+    }
+
+    checkBottom() {
+
+    }
+
+    checkLeft() {
+
+    }
+
+    checkRight() {
+
+    }
+
+    moveRight() {
+
+    }
+
+    moveLeft() {
+
+    }
+
+    moveBottom() {
+        changeRotation() {}
+    }
+}
+
+const imageSquareSize = 24;
